9bdf26f14ccd181ee8096c7970720580dddd44ca
David Rose <drose6307@gmail.com>
2019-06-10 05:18:17 -0700

    CRUD Function Complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import PostForm from './PostForm'
-import AllPost from './AllPost'
+import React, { Component } from 'react';
+import PostForm from './PostForm';
+import AllPost from './AllPost';
 
-function App() {
-  return (
-    <div className="App">
-      <PostForm />
-      <AllPost />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="navbar">
+          <h2 className="center ">Post It</h2>
+        </div>
+        <PostForm />
+        <AllPost />
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/PostForm.js
+++ src/PostForm.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-
+import { connect } from 'react-redux';
 class PostForm extends Component {
-
     handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const title = this.getTitle.value;
-        const message =  this.getMessage.value;
+        const message = this.getMessage.value;
         const data = {
-          id: new Date(),
-          title,
-          message
+            id: new Date(),
+            title,
+            message,
+            editing: false
         }
         this.props.dispatch({
-            type : 'ADD_POST',
+            type: 'ADD_POST',
             data
         })
         this.getTitle.value = '';
         this.getMessage.value = '';
     }
-
     render() {
         return (
-            <div>
-                <h1>Create Post</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <input required type="text" ref={(input)=>this.getTitle = input} placeholder="Enter Post Title"/>
-                    <br /><br />
-                    <textarea required rows="5" ref={(input)=>this.getMessage = input} cols="28" placeholder="Enter Post" />
-                    <br /><br />
+            <div className="post-container">
+                <h1 className="post_heading">Create Post</h1>
+                <form className="form" onSubmit={this.handleSubmit} >
+                    <input required type="text" ref={(input) => this.getTitle = input}
+                        placeholder="Enter Post Title" /><br /><br />
+                    <textarea required rows="5" ref={(input) => this.getMessage = input}
+                        cols="28" placeholder="Enter Post" /><br /><br />
                     <button>Post</button>
                 </form>
             </div>
